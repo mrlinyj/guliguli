b52b3002ba7ac7b60abc0fd83dd0f4e1a6d5ad05
linyongjing <linyongjing@yuanfudao.com>
2021-06-23 16:35:14 +0800

    init

---
+++ src/main/java/atguigu/guliguli/GuliguliApplication.java
@@ -0,0 +1,13 @@
+package atguigu.guliguli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuliguliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuliguliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/atguigu/guliguli/GuliguliApplicationTests.java
@@ -0,0 +1,13 @@
+package atguigu.guliguli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliguliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
